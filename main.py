d51a1b00baf0e8650dd8c67c7600656dde47cc06
6Esther22 <2917038286@qq.com>
2025-10-25 22:34:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-#weightConvert.py
-weightStr=input()
-if weightStr[-2:] in ['kg']:
-    pd=(eval(weightStr[0:-2]))*2.2046
-    print("对应的英制重量为{:.3f}磅".format(pd))
-elif weightStr[-2:] in ['pd']:
-    kg=(eval(weightStr[0:-2]))/2.2046
-    print("对应的公制重量为{:.3f}公斤".format(kg))
-else:
-    print("输入格式错误")
+initial_weight = 50  
+moon_ratio = 0.165  
+for year in range(1, 11):
+    earth_weight = initial_weight + year * 0.5
+    moon_weight = earth_weight * moon_ratio
+    print(f"第{year}年，地球上的体重：{earth_weight:.2f}kg，月球上的体重：{moon_weight:.2f}kg")
