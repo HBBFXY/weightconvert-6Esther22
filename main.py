0b8366296bf5789208133c40c56cc60e3e929e48
6Esther22 <2917038286@qq.com>
2025-10-25 22:40:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-initial_weight = 50  
-moon_ratio = 0.165  
-for year in range(1, 11):
-    earth_weight = initial_weight + year * 0.5
-    moon_weight = earth_weight * moon_ratio
-    print(f"第{year}年，地球上的体重：{earth_weight:.2f}kg，月球上的体重：{moon_weight:.2f}kg")
+weight=input()
+if weight[-2:] in ["kg"]:
+    poung = (eval(weight[0:-2]))*2.2046
+    print("对应的英制重量为{:.3f}磅".format(poung))
+elif weight[-2:]in["pd"]:
+    kg = (eval(weight[0:-2]))*0.4535
+    print("对应的公制重量为{:.3f}公斤".format(kg))
+else:
+    print("输入错误")
